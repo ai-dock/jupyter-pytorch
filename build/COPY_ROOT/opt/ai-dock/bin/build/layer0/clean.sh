13a1d143245d2e90488718b5d9906194e54e6d7c
Rob Ballantyne <rob@dynamedia.uk>
2023-12-18 01:01:12 +0000

    Add sox for torchaudio

--- build/COPY_ROOT/opt/ai-dock/bin/build/layer0/clean.sh
+++ build/COPY_ROOT/opt/ai-dock/bin/build/layer0/clean.sh
@@ -3,14 +3,3 @@
 # Tidy up and keep image small
 apt-get clean -y
 micromamba clean -ay
-
-# Remove build scripts
-scripts_dir="/opt/ai-dock/bin/build/"
-
-# Remove this layer's scripts
-rm -rf ${scripts_dir}layer0
-
-# Remove parent directory if this is the last build layer
-if [[ $(ls -l ${scripts_dir} | grep -c ^d) -eq 0 ]]; then
-    rm -rf ${scripts_dir}
-fi
